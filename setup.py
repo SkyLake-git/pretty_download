79de261c30238591af3f8427e4c3250272951ef3
SkyLake <70795425+SkyLake-git@users.noreply.github.com>
2023-03-08 21:57:23 +0900

    update for pip installable

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+	name="pretty_download",
+	version="1.0.0",
+	author="SkyLake",
+	license="MIT",
+	description="Visualizing downloads.",
+	url="https://github.com/SkyLake-git/pretty_download",
+	packages=setuptools.find_packages()
+)
